068d4d90f892f5efc37e2c5ad2d7b22a1843f56e
GAjay <devakm95@gmail.com>
2024-11-05 13:50:14 +0530

    hashrouting

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,4 +1,6 @@
 import { useCallback, useState } from "react";
+import { useLocation } from "react-router";
+import { HashLink } from 'react-router-hash-link';
 
 // const menus = [
 
@@ -11,6 +13,7 @@ import { useCallback, useState } from "react";
 // ];
 
 export default function Navbar(props:{menus:Array<{name:string, id?:string, url?:string}>, logo:string}) {
+  const {pathname}=useLocation()
   const {menus, logo}=props
   const [isOpen, setOpen] = useState(false);
 
@@ -56,13 +59,14 @@ export default function Navbar(props:{menus:Array<{name:string, id?:string, url?
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             {menus.map((item) => (
               <li>
-                <a
-                  href={item.url?item.url:`#${item.id}`}
+                <HashLink
+                  to={item.url?item.url:`${pathname}/#${item.id}`}
                   key={item.url?item.url:item.id}
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-gray md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   {item.name}
-                </a>
+                </HashLink>
+              
               </li>
             ))}
           </ul>
