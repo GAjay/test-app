84bdc8d8184cff72a98d2efd1050e92b5a4e300b
GAjay <devakm95@gmail.com>
2024-11-06 15:09:37 +0530

    remove back to top

--- src/disposable.tsx
+++ src/disposable.tsx
@@ -121,28 +121,6 @@ function Disposable() {
       <Aboutus bgColor="bg-gradient-to-r from-zinc-900 to-zinc-900"/>
       <Products />
      <ContactUs  bgColor="bg-gradient-to-r from-zinc-900 to-zinc-900"/>
-      <div className="fixed bottom-0 right-0 p-4">
-        <a
-          href="#home"
-          className="bg-gray-800 text-white rounded-full w-10 h-10 flex items-center justify-center"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 10l7-7m0 0l7 7m-7-7v18"
-            ></path>
-          </svg>
-        </a>
-      </div>
-
     </>
   );
 }
